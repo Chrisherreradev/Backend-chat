13fb1909834ac7537bb6952d62e460a433f58b6d
chris <christianherrera@live.com.mx>
2023-01-29 21:00:39 -0600

    Mensaje Class username, tipo, color variables added, set random color in controller Class

--- src/main/java/com/springboot/backend/chatAPI/Backendchat/controller/ChatController.java
+++ src/main/java/com/springboot/backend/chatAPI/Backendchat/controller/ChatController.java
@@ -6,15 +6,22 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class ChatController {
+    private String[] colores = {"red", "green", "blue","magenta","purple", "orange", "cyan"};
 
     @MessageMapping("/mensaje")
     @SendTo("/chat/mensaje")
     public Mensaje recibeMensaje(Mensaje mensaje){
         mensaje.setFecha(new Date().getTime());
-        mensaje.setTexto("Recibido por el broker: " + mensaje.getTexto());
+
+        if (mensaje.getTipo().equals("NUEVO_USUARIO")){
+            mensaje.setColor(colores[new Random().nextInt(colores.length)]);
+            mensaje.setTexto("nuevo usuario");
+        }
+
         return mensaje;
 
     }
